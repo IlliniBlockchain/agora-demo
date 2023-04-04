3fe41efb55700958ecbb7e436883e3940daebec6
kevinz420 <kzhang0305@gmail.com>
2023-04-03 23:45:13 -0500

    vite fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,7 +19,8 @@ export default defineConfig({
             NodeGlobalsPolyfillPlugin({
                 buffer: true
             })
-        ]
+        ],
+        base: '/vite-deploy-demo/'
     }
 }
 })
